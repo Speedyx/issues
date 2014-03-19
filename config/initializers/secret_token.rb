fa6a2bfb642aeb454af19a36280da5e27f8b7f80
Speedyx <cacope76@hotmail.com>
2014-03-19 19:10:54 +0100

    Issues con scaffolds

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Issues::Application.config.secret_key_base = 'f548c6744120695501aa4a4f98dd9df16999f8a62fa9e2c294b52e4d7acd6d05b2971303457f1a6fe0e0184cb63ae77cf5edefd16d49ecebd3a5a9c72fb85a5e'
